9a3f77b96f098b09267cbaeb25af12d4c2497384
weizujie <byojiaoxianz7@outlook.com>
2021-01-13 22:39:41 +0800

    finished send email

--- src/main/java/com/community/utils/MailClient.java
+++ src/main/java/com/community/utils/MailClient.java
@@ -1,2 +1,37 @@
-package com.community.utils;public class MailClient {
+package com.community.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Component
+public class MailClient {
+
+    private final JavaMailSender mailSender;
+
+    public MailClient(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void sendMail(String to, String subject, String content) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+            mailSender.send(helper.getMimeMessage());
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
