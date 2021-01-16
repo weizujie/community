ff3c1b537d6001a873c392492ba58868def1806a
weizujie <byojiaoxianz7@outlook.com>
2021-01-16 11:11:56 +0800

    update pom.xml

---
+++ src/main/java/com/community/config/KaptchaConfig.java
@@ -0,0 +1,12 @@
+package com.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Kaptcha {
+
+    @Bean
+    public 
+
+}
