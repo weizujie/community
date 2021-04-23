27309022a9fa991ced0bfe5524ee73546991d04a
weizujie <byojiaoxianz7@outlook.com>
2021-04-23 10:02:09 +0800

    Add files via upload

---
+++ community.sql
@@ -0,0 +1,108 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80022
+ Source Host           : localhost:3306
+ Source Schema         : community
+
+ Target Server Type    : MySQL
+ Target Server Version : 80022
+ File Encoding         : 65001
+
+ Date: 23/04/2021 09:59:32
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `user_id` int(0) NULL DEFAULT NULL,
+  `entity_type` int(0) NULL DEFAULT NULL,
+  `entity_id` int(0) NULL DEFAULT NULL,
+  `target_id` int(0) NULL DEFAULT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `status` int(0) NULL DEFAULT NULL,
+  `create_time` timestamp(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `index_user_id`(`user_id`) USING BTREE,
+  INDEX `index_entity_id`(`entity_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 266 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for discuss_post
+-- ----------------------------
+DROP TABLE IF EXISTS `discuss_post`;
+CREATE TABLE `discuss_post`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户id',
+  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标题',
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '内容',
+  `type` int(0) NULL DEFAULT NULL COMMENT '0-普通; 1-置顶;',
+  `status` int(0) NULL DEFAULT NULL COMMENT '0-正常; 1-精华; 2-拉黑;',
+  `create_time` timestamp(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `comment_count` int(0) NULL DEFAULT NULL COMMENT '评论数',
+  `score` double NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `index_user_id`(`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 297 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for login_ticket
+-- ----------------------------
+DROP TABLE IF EXISTS `login_ticket`;
+CREATE TABLE `login_ticket`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `user_id` int(0) NOT NULL,
+  `ticket` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `status` int(0) NULL DEFAULT 0 COMMENT '0-有效; 1-无效;',
+  `expired` timestamp(0) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `index_ticket`(`ticket`(20)) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 40 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for message
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `from_id` int(0) NULL DEFAULT NULL COMMENT '消息的发送用户',
+  `to_id` int(0) NULL DEFAULT NULL COMMENT '消息的接收用户',
+  `conversation_id` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '会话 id',
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '消息内容',
+  `status` int(0) NULL DEFAULT NULL COMMENT '0-未读;1-已读;2-删除;',
+  `create_time` timestamp(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `index_from_id`(`from_id`) USING BTREE,
+  INDEX `index_to_id`(`to_id`) USING BTREE,
+  INDEX `index_conversation_id`(`conversation_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 371 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `salt` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `type` int(0) NULL DEFAULT NULL COMMENT '0-普通用户; 1-超级管理员; 2-版主;',
+  `status` int(0) NULL DEFAULT NULL COMMENT '0-未激活; 1-已激活;',
+  `activation_code` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `header_url` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` timestamp(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `index_username`(`username`(20)) USING BTREE,
+  INDEX `index_email`(`email`(20)) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 166 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
